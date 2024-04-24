25316dc0e849e9212fb5340e217d37687f2069b0
iamj <iamj.nn@mail.ru>
2024-04-24 21:32:26 +0300

    Adding convert to menu slot

--- src/main/java/nn/iamj/borne/modules/commerce/unit/CommerceSlot.java
+++ src/main/java/nn/iamj/borne/modules/commerce/unit/CommerceSlot.java
@@ -2,13 +2,58 @@ package nn.iamj.borne.modules.commerce.unit;
 
 import lombok.Getter;
 import lombok.Setter;
+import nn.iamj.borne.Borne;
+import nn.iamj.borne.modules.commerce.utils.CommerceUtils;
+import nn.iamj.borne.modules.menu.executable.ExecutableClick;
+import nn.iamj.borne.modules.menu.slot.MenuSlot;
+import nn.iamj.borne.modules.profile.Profile;
+import nn.iamj.borne.modules.server.scheduler.Scheduler;
+import nn.iamj.borne.modules.util.component.Component;
+import nn.iamj.borne.modules.util.inventory.InventoryUtils;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
 
 @Getter @Setter
 public class CommerceSlot extends CommerceUnit {
 
     private int slot = -1;
 
-    public static CommerceSlot convert(final CommerceUnit unit) {
+    public static MenuSlot convertToMenu(final CommerceSlot slot) {
+        final MenuSlot item = new MenuSlot(slot.getDisplay());
+
+        item.setPosition(slot.getSlot());
+        item.setExecutableClick(new ExecutableClick() {
+            @Override
+            public void onLeft(final @NotNull Profile profile) {
+                final Player player = profile.asBukkit();
+
+                if (player == null) return;
+
+                if (!CommerceUtils.has(profile, slot.getPrice())) {
+                    profile.sendText(Component.text(Component.Type.ERROR, "У Вас недостаточно материалов для покупки этого товара."));
+                    return;
+                }
+
+                CommerceUtils.pay(profile, slot.getPrice());
+
+                InventoryUtils.addItems(player, slot.getStackList());
+                slot.getCommandList().forEach(command -> {
+                    final Runnable runnable = () -> Borne.getBorne().getPlugin().getServer().dispatchCommand(Bukkit.getConsoleSender(), command);
+
+                    if (!Bukkit.isPrimaryThread()) {
+                        Scheduler.handle(runnable);
+                    } else runnable.run();
+                });
+
+                profile.sendText(Component.text(Component.Type.SUCCESS, "Вы успешно приобрели этот товар."));
+            }
+        });
+
+        return item;
+    }
+
+    public static CommerceSlot convertToSlot(final CommerceUnit unit) {
         final CommerceSlot slot = new CommerceSlot();
 
         slot.setDisplay(unit.getDisplay());
